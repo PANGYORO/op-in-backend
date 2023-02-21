fb3a357972aca7a4980af863b7650995b4e55a98
ManduTheCat <krocd@naver.com>
2023-02-21 20:56:55 +0900

    init : github move

--- op-in-front/src/components/Container.jsx
+++
@@ -1,9 +0,0 @@
-import cx from "classnames";
-import React from "react";
-
-function Container({ className, children }) {
-  return <div className={cx("container", className)}>{children}</div>;
-}
-
-export default Container;
-
